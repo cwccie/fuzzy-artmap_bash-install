78e0797098f5a2f91a4c03660b5247e06298de16
cwccie <157053003+cwccie@users.noreply.github.com>
2024-01-18 16:12:28 -0600

    Update famsetup.sh

--- famsetup.sh
+++ famsetup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+#
+### Built for Ubuntu 22.04 Server 
+# https://releases.ubuntu.com/22.04.3/ubuntu-22.04.3-live-server-amd64.iso
+#
 # Set the DEBIAN_FRONTEND environment variable to noninteractive and update package information without prompts
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update -y
@@ -10,17 +13,17 @@ sudo apt update -y
 ###
 # Install net-tools for ifconfig functionality without prompts and display output
 # Remove the comment (#) from the following line if you would like to install net-tools
-sudo apt install -y net-tools
+# sudo apt install -y net-tools
 #
 # Install Samba without prompts, display output, Add share lines to the smb.conf file
 # Remove the Comment (#) from the following 7 lines to install Samba and establish a guest access share. Be aware of security rsisks before doing this.
-sudo apt install -y samba
-echo "[fam_share]" | sudo tee -a /etc/samba/smb.conf
-echo "path = $PWD" | sudo tee -a /etc/samba/smb.conf
-echo "read only = no" | sudo tee -a /etc/samba/smb.conf
-echo "guest only = yes" | sudo tee -a /etc/samba/smb.conf
-echo "guest ok = yes" | sudo tee -a /etc/samba/smb.conf
-sudo systemctl restart smbd
+# sudo apt install -y samba
+# echo "[fam_share]" | sudo tee -a /etc/samba/smb.conf
+# echo "path = $PWD" | sudo tee -a /etc/samba/smb.conf
+# echo "read only = no" | sudo tee -a /etc/samba/smb.conf
+# echo "guest only = yes" | sudo tee -a /etc/samba/smb.conf
+# echo "guest ok = yes" | sudo tee -a /etc/samba/smb.conf
+# sudo systemctl restart smbd
 #
 ####################################################################################################
 ####################################################################################################
@@ -50,11 +53,18 @@ sudo pip install tornado
 sudo pip install matplotlib
 sudo pip install ipympl
 
-# Change permissions on the famuser directory without prompts
-sudo chmod -R 777
+####################################################################################################
+####################################################################################################
+### Optional Permission Change 
+###
+# If needed, remove the comment (#) below, Change permissions on the FAM directory without prompts
+# sudo chmod -R 777
+
+####################################################################################################
+####################################################################################################
 
 # Run the Python script
 sudo python3.10 fuzzy-artmap/circle_square_test.py
 
-############################################################################
+####################################################################################################
 
